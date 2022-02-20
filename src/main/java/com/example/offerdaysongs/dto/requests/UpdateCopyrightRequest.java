19885b3ca45bb64f6e68f2f761a879d52442238e
Jegors Čemisovs <jegors.cemisovs@outlook.lv>
2022-02-20 13:23:18 +0200

    Add java bean validation annotations for UpdateCopyrightRequest

--- src/main/java/com/example/offerdaysongs/dto/requests/UpdateCopyrightRequest.java
+++ src/main/java/com/example/offerdaysongs/dto/requests/UpdateCopyrightRequest.java
@@ -3,6 +3,8 @@ package com.example.offerdaysongs.dto.requests;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.PositiveOrZero;
 import java.math.BigDecimal;
 import java.time.LocalDate;
 
@@ -11,7 +13,11 @@ import java.time.LocalDate;
 public class UpdateCopyrightRequest {
     long id;
 
+    @NotNull
+    @PositiveOrZero
     BigDecimal royalty;
+    @NotNull
     LocalDate periodStart;
+    @NotNull
     LocalDate periodEnd;
 }
